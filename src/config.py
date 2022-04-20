6c388f97bf6fb363a656003cc14a9148017ba6b8
Jaren Glenn <jarenglenn@gmail.com>
2022-04-20 13:21:51 -0600

    split config into AccessPointConfig

--- src/config.py
+++ src/config.py
@@ -9,12 +9,18 @@ from pydantic import BaseModel
 
 import util
 
+
 CONFIG_FILE = util.resolve("../config.yaml")
 
-class Config(BaseModel):
+
+class AccessPointConfig(BaseModel):
     ssid: str = "RPI-AP1"
     passwd: str = "cH4nG3M3"
     ipv4subnet: str = "192.168.0.1/28"
+
+
+class Config(BaseModel):
+    access_point: AccessPointConfig()
 
     def load_file(self, path) -> "Config":
         with open(path) as f:
@@ -22,8 +28,10 @@ class Config(BaseModel):
 
         return self.parse_obj(yaml.load(contents, Loader=yaml.CLoader))
 
+
 if os.path.isfile(CONFIG_FILE):
     config: Config = Config.load_file(Config, CONFIG_FILE)
 else:
     config = Config()
 
+print(config.access_point.ssid)
