433062017d4ab65bea8a68feab9b3f676fa1e102
Jaren Glenn <jarenglenn@gmail.com>
2022-04-27 12:59:08 -0600

    actaully fixed sudo bug this time

--- src/util.py
+++ src/util.py
@@ -39,6 +39,6 @@ def validate_cmd(process: sp.CompletedProcess[bytes], error_msg: str = None) -> 
 
 def install_pkg(pkg_name: str, service: str = "apt-get", sudo: bool = False, *args):
     """Installs a system package. Uses apt-get by default."""
-    sudo_str = "" if sudo else "sudo"
+    sudo_str = "" if not sudo else "sudo"
     install_cmd = sp.run([sudo_str, service, "install", pkg_name, *args])
     validate_cmd(install_cmd)
